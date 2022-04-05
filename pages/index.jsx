f185e1f2d4229a487cd30bfe5082354e3cc659e7
Zak Nesler <zak@nesler.dev>
2022-04-05 18:16:31 -0400

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="p-12">
+        <h1 className="text-xl font-bold">Hello, Next.js!</h1>
+
+        <div className="max-w-lg pt-3 text-lg leading-relaxed text-gray-600">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut quas
+          voluptatem cumque atque consequuntur culpa, dolore repudiandae fugiat
+          quidem perspiciatis velit iure corporis nesciunt impedit explicabo
+          blanditiis cum quae natus.
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './components/**/*.{js,jsx,ts,tsx,mdx}',
+    './pages/**/*.{js,jsx,ts,tsx,mdx}',
+    './lib/**/*.{js,jsx,ts,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
