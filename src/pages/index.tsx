d0bf37ef34242705f856de21d19167573ecc2410
Zak Nesler <zak@nesler.dev>
2022-06-13 16:49:58 -0400

    Upgrade packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,8 @@ export default function Index() {
       </Head>
 
       <main className="p-12">
-        <h1 className="text-xl font-bold">Hello, Next.js + TypeScript.</h1>
-
-        <div className="max-w-lg pt-3 text-lg leading-relaxed text-gray-600">
+        <h1 className="text-xl font-bold">Next.js + TypeScript + Tailwind</h1>
+        <div className="max-w-lg pt-4 text-lg leading-relaxed text-gray-600">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut quas
           voluptatem cumque atque consequuntur culpa, dolore repudiandae fugiat
           quidem perspiciatis velit iure corporis nesciunt impedit explicabo
